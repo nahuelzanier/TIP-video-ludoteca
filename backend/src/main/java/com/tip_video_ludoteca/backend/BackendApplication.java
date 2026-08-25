401cda4dee92af8baf3e24a2efa01df5d22bdd81
nahuelzanier <nahuelpabloz@gmail.com>
2026-08-25 18:04:49 -0300

    Initial project setup

---
+++ backend/src/main/java/com/tip_video_ludoteca/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.tip_video_ludoteca.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tip_video_ludoteca/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tip_video_ludoteca.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
