e6e0e34304d051ffa95b9a40ea4b2313c54a10ee
Lucas Sagnuinetti <lucas.e.sanguinetti@gmail.com>
2026-09-22 16:20:07 -0300

    agregada la navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
+import Perfil from "./pages/Perfil/Perfil";
+import Configuracion from "./pages/Configuracion/Configuracion";
+import Foro from "./pages/Foro/Foro";
+import JuegoAleatorio from "./pages/JuegoAleatorio/JuegoAleatorio";
+import CerrarSesion from "./pages/CerrarSesion/CerrarSesion";
 
 function App() {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/game/:id" element={<Game />} />
+                <Route path="/perfil" element={<Perfil />} />
+                <Route path="/configuracion" element={<Configuracion />} />
+                <Route path="/foro" element={<Foro />} />
+                <Route path="/juego-aleatorio" element={<JuegoAleatorio />} />
+                <Route path="/logout" element={<CerrarSesion />} />
             </Routes>
         </BrowserRouter>
     );
